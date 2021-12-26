0d26cf7b415e912ec5e8870ff79924cbc07839ce
shoshana lagasa <shoshanalagasa850@gmail.com>
2021-12-25 22:25:09 -0600

    capstone

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,8 +1,15 @@
 import html from "html-literal";
 import home from "../../Images/home.jpg";
 import home2 from "../../Images/home2.jpg";
+ const kelvinToFahrenheit = kelvinTemp =>
+  Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
 
 export default () => html`
+  <h3>
+    Temperature in ${st.weather.city} is
+    ${kelvinToFahrenheit(st.weather.temp)}F. It feels like
+    ${kelvinToFahrenheit(st.weather.feelsLike)}F.
+  </h3>
   <div class="slideshow-container">
     <div class="mySlides fade">
       <img src="${home}" style="width:90%" />
--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 import { Header, Nav, Main, Footer } from "./components";
 import * as state from "./store";
 import axios from "axios";
+
 import Navigo from "navigo";
 import { capitalize } from "lodash";
 
-const router = new Navigo(window.location.origin);
+import dotenv from "dotenv";
+dotenv.config();
 
-router
-  .on({
-    "/": () => render(state.Home),
-    ":page": params => render(state[capitalize(params.page)])
-  })
-  .resolve();
+const router = new Navigo(window.location.origin);
 
 function render(st) {
   document.getElementById("root").innerHTML = `
@@ -59,24 +56,34 @@ function showSlides() {
   dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
-// router
-// router.hooks({
-//   before: (done, params) => {
-//     const page =
-//       params && params.hasOwnProperty("page")
-//         ? capitalize(params.page)
-//         : "Home";
-//     if (page === "Home") {
-//       axios
-//         .get(response => {
-//           state.Home.weather = {};
-//           state.Home.weather.city = response.data.name;
-//           state.Home.weather.temp = response.data.main.temp;
-//           state.Home.weather.feelsLike = response.data.main.feels_like;
-//           state.Home.weather.description = response.data.weather[0].main;
-//           done();
-// //         })
-//         .catch(err => console.log(err));
-//     }
-//   }
-// });
+
+// creting weather API
+router.hooks({
+  before: (done, params) => {
+    const page =
+      params && params.hasOwnProperty("page")
+        ? capitalize(params.page)
+        : "Home";
+    if (page === "Home") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st.%20louis`
+        )
+        .then(response => {
+          state.Home.weather = {};
+          state.Home.weather.city = response.data.name;
+          state.Home.weather.temp = response.data.main.temp;
+          state.Home.weather.feelsLike = response.data.main.feels_like;
+          state.Home.weather.description = response.data.weather[0].main;
+          done();
+        })
+        .catch(err => console.log(err));
+    }
+  }
+});
+router
+  .on({
+    "/": () => render(state.Home),
+    ":page": params => render(state[capitalize(params.page)])
+  })
+  .resolve();
